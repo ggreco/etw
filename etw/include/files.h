b9a0ee244064a28a9531e36259c92ea433f8cb96
sammy <sammy@d0e6f50d-c539-0410-9ac7-97d2fe49c688>
2006-05-19 18:55:33 +0000

    * Oops, fixed a typo in include/files.h.

--- etw/include/files.h
+++ etw/include/files.h
@@ -1,19 +1,50 @@
 #ifndef FILES_H
-
 #define FILES_H
 
-// use on boolean to handle them as bytes
-#define WRITE_BOOL(var, file) { unsigned char c; c = var; fwrite(&c, sizeof(c), 1, file); }
-#define READ_BOOL(var, file) { unsigned char c; fread(&c, sizeof(c), 1, file); var = c; }
+/* use on boolean to handle them as bytes */
+#define WRITE_BOOL(var, file) { \
+    uint8_t c = var; \
+    fwrite(&c, sizeof(c), 1, file); \
+}
 
-// use on datas that need swapping
-#define WRITE_LONG(var, file) { ULONG c; c = var; SWAP_LONG(c); fwrite(&c, sizeof(c), 1, file); }
-#define WRITE_WORD(var, file) { UWORD c; c = var; SWAP_WORD(c); fwrite(&c, sizeof(c), 1, file); }
-#define READ_WORD(var, file) { UWORD c;  fread(&c, sizeof(c), 1, file); SWAP_WORD(c); var = c; }
-#define READ_LONG(var, file) { ULONG c;  fread(&c, sizeof(c), 1, file); SWAP_LONG(c); var = c; }
+#define READ_BOOL(var, file) { \
+    uint8_t c; \
+    fread(&c, sizeof(c), 1, file); \
+    var = c; \
+}
 
-// these can be used for datas that doesn't need swapping
-#define WRITE_DATA(var, file) fwrite(&var, sizeof(var), 1, file)
-#define READ_DATA(var, file) fread(&var, sizeof(var), 1, file)
+/* use on data that needs swapping */
+#define WRITE_LONG(var, file) { \
+    uint32_t c = var; \
+    SWAP_LONG(c); \
+    fwrite(&c, sizeof(c), 1, file); \
+}
+
+#define WRITE_WORD(var, file) { \
+    uint16_t c = var; \
+    SWAP_WORD(c); \
+    fwrite(&c, sizeof(c), 1, file); \
+}
+
+#define READ_WORD(var, file) { \
+    uint16_t c; \
+    fread(&c, sizeof(c), 1, file); \
+    SWAP_WORD(c); \
+    var = c; \
+}
+
+#define READ_LONG(var, file) { \
+    uint32_t c; \
+    fread(&c, sizeof(c), 1, file); \
+    SWAP_LONG(c); \
+    var = c; \
+}
+
+/* these can be used for data that doesn't need swapping */
+#define WRITE_DATA(var, file) \
+    fwrite(&var, sizeof(var), 1, file)
+
+#define READ_DATA(var, file) \
+    fread(&var, sizeof(var), 1, file)
 
 #endif
