39db8d49315a277de003867f705f0e6dbddd2b24
sammy <sammy@d0e6f50d-c539-0410-9ac7-97d2fe49c688>
2007-10-14 21:05:13 +0000

    * Make teams.c 64-bit compliant. Now we can start a game on amd64!

--- trunk/etw/include/squadre.h
+++ trunk/etw/include/squadre.h
@@ -7,23 +7,23 @@
 
 // Flags dei giocatori...
 
-#define P_DIFESA	1
-#define P_CENTRO	2
-#define P_ATTACCO	4
-#define P_DESTRO	8
-#define P_MANCINO	16
-#define P_PELLE_NERA		32
+#define P_DIFESA    1
+#define P_CENTRO    2
+#define P_ATTACCO    4
+#define P_DESTRO    8
+#define P_MANCINO    16
+#define P_PELLE_NERA        32
 
 // Flags delle squadre
 
-#define SERIE_B	1
-#define SERIE_C	2
+#define SERIE_B    1
+#define SERIE_C    2
 #define SERIE_D 4
 
-#define NO_MANAGER	0
-#define MANAGER_ONLY	1
-#define PLAYERMANAGER	2
-#define ROLEPLAYER	3
+#define NO_MANAGER    0
+#define MANAGER_ONLY    1
+#define PLAYERMANAGER    2
+#define ROLEPLAYER    3
 
 #define CAMP_CUP 0
 #define CAMP_LEAGUE 1
@@ -32,82 +32,82 @@
 
 struct Maglia
 {
-	char Tipo;
-	char Colore0;
-	char Colore1;
-	char Colore2;
+    uint8_t Tipo;
+    uint8_t Colore0;
+    uint8_t Colore1;
+    uint8_t Colore2;
 };
 
 struct Giocatore_Disk
 {
-	char Nome[20];
-	char Cognome[20];
-	unsigned long valore;
-	char Numero,Velocita,Contrasto,Tiro;
-	char Durata,Resistenza,Prontezza,Nazionalita;
-	char Creativita,Tecnica;
-	char Eta,Infortuni,Ammonizioni,Posizioni;
+    char Nome[20];
+    char Cognome[20];
+    uint32_t valore;
+    uint8_t Numero, Velocita, Contrasto, Tiro;
+    uint8_t Durata, Resistenza, Prontezza, Nazionalita;
+    uint8_t Creativita, Tecnica;
+    uint8_t Eta, Infortuni, Ammonizioni, Posizioni;
 };
 
 struct Portiere_Disk
 {
-	char Nome[20];
-	char Cognome[20];
-	unsigned long valore;
-	char Numero,Velocita,Parata,Attenzione;
-	char Nazionalita,Eta,Infortuni,Flags;
+    char Nome[20];
+    char Cognome[20];
+    uint32_t valore;
+    uint8_t Numero, Velocita, Parata, Attenzione;
+    uint8_t Nazionalita, Eta, Infortuni, Flags;
 };
 
 struct Squadra_Disk
 {
-	unsigned long disponibilita;
-	char NumeroGiocatori,NumeroPortieri,Nazione,Flags;
-	struct Maglia maglie[2];
-	char Tattiche[3][16];
-	char nome[52];
-	char allenatore[52];
-	struct Portiere_Disk portiere[3];
-	struct Giocatore_Disk giocatore[21];	
+    uint32_t disponibilita;
+    uint8_t NumeroGiocatori, NumeroPortieri, Nazione, Flags;
+    struct Maglia maglie[2];
+    char Tattiche[3][16];
+    char nome[52];
+    char allenatore[52];
+    struct Portiere_Disk portiere[3];
+    struct Giocatore_Disk giocatore[21];    
 };
 
 struct Campionato_Disk
 {
-	char Nome[64];
-	char Tipo, Scontri;
-	char Vittoria,Pareggio,Sconfitta; // Punti assegnati nei vari casi
-	char nteams;
+    char Nome[64];
+    char Tipo, Scontri;
+    char Vittoria,Pareggio,Sconfitta; // Punti assegnati nei vari casi
+    char nteams;
 };
 
 struct Controlled_Disk
 {
-	WORD ManagerType;
-	UBYTE ControlType;
-	UBYTE Joystick;
-	UBYTE Player;
+    WORD ManagerType;
+    UBYTE ControlType;
+    UBYTE Joystick;
+    UBYTE Player;
 };
 
 struct Storia
 {
-	UBYTE Posizione,Punti,Vittorie,Sconfitte,Pareggi,GolFatti,GolSubiti;
-	BYTE TurnoEuropea;
-	BYTE TurnoNazionale;
+    UBYTE Posizione,Punti,Vittorie,Sconfitte,Pareggi,GolFatti,GolSubiti;
+    BYTE TurnoEuropea;
+    BYTE TurnoNazionale;
 };
 
 struct Manager_Disk
 {
-	WORD Punteggio;
-	struct Storia storia[16];
+    WORD Punteggio;
+    struct Storia storia[16];
 };
 
 struct DatiSquadra_Disk
 {
-	unsigned short Punti; // Punti della squadra, negativo significa TURNO.
-	BOOL Controllata; // Punteggio manager, solo se "Career"
+    unsigned short Punti; // Punti della squadra, negativo significa TURNO.
+    BOOL Controllata; // Punteggio manager, solo se "Career"
 // Classifica
-	UBYTE GolFatti,GolSubiti,Giocate;
-	UBYTE Vittorie,Pareggi,Sconfitte;
-	BYTE TurnoEuropea;
-	BYTE TurnoNazionale;
+    UBYTE GolFatti,GolSubiti,Giocate;
+    UBYTE Vittorie,Pareggi,Sconfitte;
+    BYTE TurnoEuropea;
+    BYTE TurnoNazionale;
 };
 
 void ReadTeam(FILE *, struct Squadra_Disk *);
