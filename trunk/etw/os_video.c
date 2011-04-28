3949df1c4d7f0a88e6f206bfa77a98797d869ade
ggreco <ggreco@d0e6f50d-c539-0410-9ac7-97d2fe49c688>
2011-04-28 14:15:00 +0000

    - Iphone fix

--- trunk/etw/os_video.c
+++ trunk/etw/os_video.c
@@ -11,11 +11,15 @@ extern int Colors;
 extern BOOL use_width,use_height,wb_game,use_direct;
 BOOL triple_buffering=FALSE,wpa8=FALSE,overscan=0;
 
-#ifndef IPHONE
 SDL_Color SDL_palette[256];
-#else
-uint16_t SDL_palette[256];
 
+#ifdef IPHONE
+#define SCREEN_DEPTH 24
+
+uint16_t palette16[256];
+
+// no more needed since iphone is able to rotate the display by itself
+#if 0
 void blitLandscapeScreenRect8bpp(uint16_t *dst)
 {
     uint8_t *src = main_bitmap;
@@ -29,28 +33,51 @@ void blitLandscapeScreenRect8bpp(uint16_t *dst)
 		src += srcmod;
 	}
 }
+#endif
+void blitScreen16(uint16_t *dst)
+{
+    int srcmod = bitmap_width;
+    uint8_t *src = main_bitmap;    
+    int x, y;
 
+    for (y = bitmap_height; y > 0; y--)
+    	for (x = bitmap_width; x > 0; x--)
+			*(dst++) = palette16[*src++];
+}
+
+void blitScreen32(uint32_t *dst)
+{
+    int srcmod = bitmap_width;
+    uint8_t *src = main_bitmap;    
+    int x, y;
+
+    for (y = bitmap_height; y > 0; y--)
+    	for (x = bitmap_width; x > 0; x--)
+			*(dst++) = (uint32_t)(SDL_palette[*src++]);
+}
+
+#else
+#define SCREEN_DEPTH 8
 #endif
 
 void AdjustSDLPalette(void)
 {
     int i;
 
-#ifndef IPHONE
     for(i=0;i<32;i++) {
-        SDL_palette[224+i].r=SDL_palette[i].r*2/3;
-        SDL_palette[224+i].g=SDL_palette[i].g*2/3;
-        SDL_palette[224+i].b=SDL_palette[i].b*2/3;
-    }
-    SDL_SetColors(screen,SDL_palette,0,256);
-#else
-    for(i = 0; i < 32; i++) {
+#ifdef IPHONE
         int r = (SDL_palette[i] >> 11) * 2 / 3,
             g = ((SDL_palette[i] >> 6) & 0x3f) * 2 / 3,
             b = (SDL_palette[i] & 0x1f) * 2 / 3;
 
-        SDL_palette[224 + i] = (r << 11) | (g << 5) | b;
+        palette16[224 + i] = (r << 11) | (g << 5) | b;
+#endif
+        SDL_palette[224+i].r=SDL_palette[i].r*2/3;
+        SDL_palette[224+i].g=SDL_palette[i].g*2/3;
+        SDL_palette[224+i].b=SDL_palette[i].b*2/3;
     }
+#ifndef IPHONE
+    SDL_SetColors(screen,SDL_palette,0,256);
 #endif
 }
 
@@ -65,7 +92,7 @@ void ResizeWin(SDL_Event *event)
 
 //    os_resize(event);
 
-    if(!(screen=SDL_SetVideoMode(event->resize.w,event->resize.h,8,SDL_SWSURFACE|SDL_RESIZABLE)))
+    if(!(screen=SDL_SetVideoMode(event->resize.w,event->resize.h, SCREEN_DEPTH, SDL_SWSURFACE|SDL_RESIZABLE)))
     {
         screen=temp;
         return;
@@ -85,7 +112,9 @@ void ResizeWin(SDL_Event *event)
         return;
     }
 
+#ifndef IPHONE
     SDL_SetColors(screen,SDL_palette,0,256);
+#endif
 
     if(!scaling)
     {
@@ -162,25 +191,32 @@ BOOL window_open(void)
 
 void os_set_color(int color,int r ,int g, int b)
 {
-#ifndef IPHONE
     SDL_Color c;
     c.b=(unsigned char)b;
     c.g=(unsigned char)g;
     c.r=(unsigned char)r;
     SDL_palette[color]=c; // copio x il resize dopo!
 
+#ifndef IPHONE
     SDL_SetColors(screen,&c,color,1);
 #else
-    SDL_palette[color] = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3);
+    palette16[color] = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3);
 #endif
 //    D(bug("Eseguita setcolor(%ld, r%ld,g%ld,b%ld)\n",color,r,g,b));
 }
 
 void OpenTheScreen(void)
 {
-#ifndef WINCE
+    screen_depth = 8;
+#ifdef IPHONE
+    screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, SCREEN_DEPTH, SDL_SWSURFACE|SDL_RESIZABLE);   
+    force_single = TRUE;
+    double_buffering = FALSE;
+    wb_game = FALSE;
+    scaling = FALSE;
+#elif !defined(WINCE)
     if(wb_game) {
-        screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 8, SDL_SWSURFACE|SDL_RESIZABLE);   
+        screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, SCREEN_DEPTH, SDL_SWSURFACE|SDL_RESIZABLE);   
 
         force_single=TRUE;
         double_buffering=FALSE;
@@ -196,14 +232,12 @@ void OpenTheScreen(void)
             double_buffering=FALSE;
 
         if(double_buffering)
-            screen =SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 8, SDL_HWSURFACE|SDL_FULLSCREEN|SDL_DOUBLEBUF);
+            screen =SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, SCREEN_DEPTH, SDL_HWSURFACE|SDL_FULLSCREEN|SDL_DOUBLEBUF);
         else
-            screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 8, SDL_HWSURFACE|SDL_FULLSCREEN); 
+            screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, SCREEN_DEPTH, SDL_HWSURFACE|SDL_FULLSCREEN); 
     }
 
     if(screen) {
-        screen_depth = 8;
-
         if(!wb_game)
             SDL_ShowCursor(0);
 
@@ -212,7 +246,6 @@ void OpenTheScreen(void)
         D(bug("Fine InitAnimSystem!\n"));
     }
 #else
-    screen_depth = 8;
     force_single = TRUE; double_buffering = FALSE; wb_game = FALSE;
     scaling = FALSE;
     screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, 8, SDL_SWSURFACE|SDL_FULLSCREEN);
@@ -222,12 +255,6 @@ void OpenTheScreen(void)
 void os_set_window_frame(void)
 {
     WINDOW_WIDTH=min(screen->w,FIELD_WIDTH);
-/*
- non serve +
-
-    if(WINDOW_WIDTH!=((WINDOW_WIDTH>>5)<<5))
-        WINDOW_WIDTH=(WINDOW_WIDTH>>5)<<5;
-*/
     WINDOW_HEIGHT=min(screen->h,FIELD_HEIGHT);
 }
 
@@ -256,32 +283,31 @@ int os_get_innerheight(void)
     return screen->h;
 }
 
-void os_load_palette(uint32_t *palette)
+void os_load_palette(uint32_t *pal)
 {
     int colornum, first, i;
-    colornum = palette[0] >> 16;
-    first = palette[0] & 0xffff;
+    colornum = pal[0] >> 16;
+    first = pal[0] & 0xffff;
 
     if(colornum > 256)
         return;
 
-#ifndef IPHONE    
     for(i = 0; i < colornum; i++)
     {
-        SDL_palette[i + first].r = palette[1 + i *3] >> 24;
-        SDL_palette[i + first].g = palette[1 + i *3 + 1] >> 24;
-        SDL_palette[i + first].b = palette[1 + i *3 + 2] >> 24;
+#ifdef IPHONE    
+        int r = pal[1 + i *3] >> 27,
+            g = pal[1 + i *3 + 1] >> 26,
+            b = pal[1 + i *3 + 2] >> 27;
+
+        palette16[i + first] = (r << 11) | (g << 5) | b;
+#endif
+        SDL_palette[i + first].r = pal[1 + i *3] >> 24;
+        SDL_palette[i + first].g = pal[1 + i *3 + 1] >> 24;
+        SDL_palette[i + first].b = pal[1 + i *3 + 2] >> 24;
     }
 
+#ifndef IPHONE    
     SDL_SetColors(screen, &SDL_palette[first], first, colornum);
-#else
-    for(i = 0; i < colornum; i++) {
-        int r = palette[1 + i *3] >> 27,
-            g = palette[1 + i *3 + 1] >> 26,
-            b = palette[1 + i *3 + 2] >> 27;
-
-        SDL_palette[i + first] = (r << 11) | (g << 5) | b;
-    }
 #endif
 }
 
@@ -309,8 +335,10 @@ void ScreenSwap(void)
                 dest += screen->pitch;
             }
         }
-#else
-        blitLandscapeScreenRect8bpp(screen->pixels);
+#elif SCREEN_DEPTH == 16
+        blitScreen16(screen->pixels);
+#else 
+        blitScreen32(screen->pixels);
 #endif
         os_unlock_bitmap();
 
