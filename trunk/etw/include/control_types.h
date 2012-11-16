7d6e5895fdc02a8d846bbe414fe0c8c768497ae3
ggreco <ggreco@d0e6f50d-c539-0410-9ac7-97d2fe49c688>
2012-11-16 17:29:57 +0000

    - Missing file in previous commit

---
+++ trunk/etw/include/control_types.h
@@ -0,0 +1,13 @@
+#ifndef CONTROL_TYPES_H
+#define CONTROL_TYPES_H
+
+#define CTRL_JOY    0
+#define CTRL_JOYPAD    1
+#define CTRL_JOY2B    2
+#define CTRL_KEY_1    3
+#define CTRL_KEY_2    4
+#define CTRL_TOUCH    5
+
+#define CONTROLS    (CTRL_TOUCH+2)
+
+#endif
