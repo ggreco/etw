49a2d538e96d40a4558d6a2e44ce5d2e89480134
ggreco <ggreco@d0e6f50d-c539-0410-9ac7-97d2fe49c688>
2012-11-22 12:07:15 +0000

    Fix to vjoy

--- trunk/etw/vjoy.cpp
+++ trunk/etw/vjoy.cpp
@@ -27,10 +27,10 @@ void init_touch(SDL_Window *win)
     touch->add_button("mobile/pause-normal.bmp", "mobile/pause-pressed.bmp", WINDOW_WIDTH - 66, 2);
 
     replay_touch = new TouchControl(win);
-    touch->add_button("mobile/slow-normal.bmp", "mobile/slow-pressed.bmp", (WINDOW_WIDTH - 52 * 4) / 2, WINDOW_HEIGHT - 52);
-    touch->add_button("mobile/rpause-normal.bmp", "mobile/rpause-pressed.bmp", (WINDOW_WIDTH - 52 * 4) / 2 + 52, WINDOW_HEIGHT - 52);
-    touch->add_button("mobile/record-normal.bmp", "mobile/record-pressed.bmp", (WINDOW_WIDTH - 52 * 4) / 2 + 52 * 2, WINDOW_HEIGHT - 52);
-    touch->add_button("mobile/stop-normal.bmp", "mobile/stop-pressed.bmp", (WINDOW_WIDTH - 52 * 4) / 2 + 52 * 3, WINDOW_HEIGHT - 52);
+    replay_touch->add_button("mobile/slow-normal.bmp", "mobile/slow-pressed.bmp", (WINDOW_WIDTH - 52 * 4) / 2, WINDOW_HEIGHT - 52);
+    replay_touch->add_button("mobile/rpause-normal.bmp", "mobile/rpause-pressed.bmp", (WINDOW_WIDTH - 52 * 4) / 2 + 52, WINDOW_HEIGHT - 52);
+    replay_touch->add_button("mobile/record-normal.bmp", "mobile/record-pressed.bmp", (WINDOW_WIDTH - 52 * 4) / 2 + 52 * 2, WINDOW_HEIGHT - 52);
+    replay_touch->add_button("mobile/stop-normal.bmp", "mobile/stop-pressed.bmp", (WINDOW_WIDTH - 52 * 4) / 2 + 52 * 3, WINDOW_HEIGHT - 52);
 
     D(bug("Touch interfaces initialized\n"));
 }
@@ -67,22 +67,24 @@ int check_replay_touch()
 
     int res = replay_touch->iteration();
 
-    if (res  & (TouchControl::BUTTON_1|TouchControl::BUTTONUP)) {
-        if(!slow_motion) {
-            MY_CLOCKS_PER_SEC_50<<=2;
-            slow_motion=TRUE;
+    if (res & TouchControl::BUTTONUP) {
+        if (res  & TouchControl::BUTTON_1) {
+            if(!slow_motion) {
+                MY_CLOCKS_PER_SEC_50<<=2;
+                slow_motion=TRUE;
+            }
+            else {
+                slow_motion=FALSE;
+                MY_CLOCKS_PER_SEC_50>>=2;
+            }
         }
-        else {
-            slow_motion=FALSE;
-            MY_CLOCKS_PER_SEC_50>>=2;
-        }        
+        if (res & TouchControl::BUTTON_3)
+            SaveReplay();
+        
+        if (res & TouchControl::BUTTON_4)
+            rc = FALSE;
     }
-    if (res & (TouchControl::BUTTON_3|TouchControl::BUTTONUP)) 
-        SaveReplay();
     
-    if (res & (TouchControl::BUTTON_4|TouchControl::BUTTONUP)) 
-        rc = FALSE;
-   
     return rc;
 }
 
@@ -102,12 +104,12 @@ uint32_t MyReadTouchPort(uint32_t l)
     if (team_t *s = find_controlled_team()) {
         if ((pl->gioc_palla && pl->gioc_palla->team != s) ||
             (!pl->gioc_palla)) {
-            if (res & (TouchControl::FREE_TOUCH|TouchControl::BUTTONUP)) {
+            if ((res & (TouchControl::FREE_TOUCH|TouchControl::BUTTONUP)) == (TouchControl::FREE_TOUCH|TouchControl::BUTTONUP)) {
                 // we can change the active player to the nearest one...
                 player_t *g2=FindNearest(s, (touch->touch_x() + field_x) << 3 ,
                                             (touch->touch_y() + field_y) << 3);
 
-                D(bug("Detected free touch at %d,%d, changing player from %s to %s", 
+                D(bug("Detected free touch at %d,%d, changing player from %s to %s\n", 
                             touch->touch_x(), touch->touch_y(), 
                             s->attivo ? s->attivo->name : "NONE",
                             g2 ? g2->name : "NONE"));
