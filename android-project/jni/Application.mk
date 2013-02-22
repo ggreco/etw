a554b065e6bcf2119f30b42b88d67ad2ffa8f666
ggreco <ggreco@d0e6f50d-c539-0410-9ac7-97d2fe49c688>
2013-02-22 16:40:10 +0000

    Fixes to android inapp purchases

--- android-project/jni/Application.mk
+++ android-project/jni/Application.mk
@@ -1,4 +1,5 @@
 
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+APP_PLATFORM := android-10
 APP_STL := stlport_static 
