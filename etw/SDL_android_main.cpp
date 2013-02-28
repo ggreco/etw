c9025e6b505a5873f78b4c5ff2d9673610be7b6b
ggreco <ggreco@d0e6f50d-c539-0410-9ac7-97d2fe49c688>
2013-02-28 08:59:40 +0000

    If the game goes in pause because it's sent in the background, ads are NOT disabled.

--- etw/SDL_android_main.cpp
+++ etw/SDL_android_main.cpp
@@ -102,44 +102,33 @@ extern "C" void hide_ads() {
 
 extern "C" {
     void Java_org_libsdl_app_SDLActivity_nativeRunAudioThread(JNIEnv* env, jclass cls);
-    void Java_org_libsdl_app_SDLActivity_onNativeResize(JNIEnv* env, jclass jcls,
-                                    jint width, jint height, jint format);
+    void Java_org_libsdl_app_SDLActivity_onNativeResize(JNIEnv* env, jclass jcls, jint width, jint height, jint format);
     void Java_org_libsdl_app_SDLActivity_onNativeKeyDown(JNIEnv* env, jclass jcls, jint keycode);
     void Java_org_libsdl_app_SDLActivity_onNativeKeyUp(JNIEnv* env, jclass jcls, jint keycode);
-    void Java_org_libsdl_app_SDLActivity_onNativeTouch(JNIEnv* env, jclass jcls,
-                                    jint touch_device_id_in, jint pointer_finger_id_in,
-                                    jint action, jfloat x, jfloat y, jfloat p);
-    void Java_org_libsdl_app_SDLActivity_onNativeAccel(JNIEnv* env, jclass jcls,
-                                    jfloat x, jfloat y, jfloat z);
+    void Java_org_libsdl_app_SDLActivity_onNativeTouch(JNIEnv* env, jclass jcls, jint touch_device_id_in, jint pointer_finger_id_in, jint action, jfloat x, jfloat y, jfloat p);
+    void Java_org_libsdl_app_SDLActivity_onNativeAccel(JNIEnv* env, jclass jcls, jfloat x, jfloat y, jfloat z);
     void Java_org_libsdl_app_SDLActivity_nativeQuit(JNIEnv* env, jclass cls);
     void Java_org_libsdl_app_SDLActivity_nativeRunAudioThread(JNIEnv* env, jclass cls);
     void Java_org_libsdl_app_SDLActivity_nativePause(JNIEnv* env, jclass cls);
     void Java_org_libsdl_app_SDLActivity_nativeResume(JNIEnv* env, jclass cls);
 };
 
-extern "C" void Java_org_ggsoft_etw_SDLActivity_load(JNIEnv* env, jclass jcls, jobject obj)
-{
+extern "C" void Java_org_ggsoft_etw_SDLActivity_load(JNIEnv* env, jclass jcls, jobject obj) {
     asset_mgr = AAssetManager_fromJava(env, obj); 
 }
 
-extern "C" void Java_org_ggsoft_etw_SDLActivity_setInches(JNIEnv* env, jclass jcls, jfloat wi, jfloat hi)
-{
+extern "C" void Java_org_ggsoft_etw_SDLActivity_setInches(JNIEnv* env, jclass jcls, jfloat wi, jfloat hi) {
     display_width_inches = wi;
     display_height_inches = hi;
 }
                                     
 // Resize
-extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeResize(
-                                    JNIEnv* env, jclass jcls,
-                                    jint width, jint height, jint format)
-{
+extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeResize( JNIEnv* env, jclass jcls, jint width, jint height, jint format) {
     Java_org_libsdl_app_SDLActivity_onNativeResize(env, jcls, width, height, format);
 }
  
 // Keydown
-extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeKeyDown(
-                                    JNIEnv* env, jclass jcls, jint keycode)
-{
+extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeKeyDown(JNIEnv* env, jclass jcls, jint keycode) {
     Java_org_libsdl_app_SDLActivity_onNativeKeyDown(env, jcls, keycode);
 }
  
@@ -155,39 +144,26 @@ extern "C" void Java_org_ggsoft_etw_SDLActivity_nativeResume(JNIEnv* env, jclass
 }
 
 // Keyup
-extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeKeyUp(
-                                    JNIEnv* env, jclass jcls, jint keycode)
-{
+extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeKeyUp(JNIEnv* env, jclass jcls, jint keycode) {
     Java_org_libsdl_app_SDLActivity_onNativeKeyUp(env, jcls, keycode);
 }
  
 // Touch
-extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeTouch(
-                                    JNIEnv* env, jclass jcls,
-                                    jint touch_device_id_in, jint pointer_finger_id_in,
-                                    jint action, jfloat x, jfloat y, jfloat p)
-{
+extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeTouch(JNIEnv* env, jclass jcls, jint touch_device_id_in, jint pointer_finger_id_in, jint action, jfloat x, jfloat y, jfloat p) {
     Java_org_libsdl_app_SDLActivity_onNativeTouch(env, jcls, touch_device_id_in, pointer_finger_id_in, action, x, y, p);
 }
  
 // Accelerometer
-extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeAccel(
-                                    JNIEnv* env, jclass jcls,
-                                    jfloat x, jfloat y, jfloat z)
-{
+extern "C" void Java_org_ggsoft_etw_SDLActivity_onNativeAccel(JNIEnv* env, jclass jcls, jfloat x, jfloat y, jfloat z) {
      Java_org_libsdl_app_SDLActivity_onNativeAccel(env, jcls, x, y, z);
 }
  
 // Quit
-extern "C" void Java_org_ggsoft_etw_SDLActivity_nativeQuit(
-                                    JNIEnv* env, jclass cls)
-{
+extern "C" void Java_org_ggsoft_etw_SDLActivity_nativeQuit(JNIEnv* env, jclass cls) {
     Java_org_libsdl_app_SDLActivity_nativeQuit(env, cls);
 }
  
-extern "C" void Java_org_ggsoft_etw_SDLActivity_nativeRunAudioThread(
-                                    JNIEnv* env, jclass cls)
-{
+extern "C" void Java_org_ggsoft_etw_SDLActivity_nativeRunAudioThread(JNIEnv* env, jclass cls) {
     Java_org_libsdl_app_SDLActivity_nativeRunAudioThread(env, cls);
 }
 
