e330112d95f6bdb7127e4c788f5f493edc9b17c8
ggreco <ggreco@d0e6f50d-c539-0410-9ac7-97d2fe49c688>
2013-03-08 15:39:21 +0000

    iAd integration

--- trunk/etw/Banner.m
+++ trunk/etw/Banner.m
@@ -1,15 +1,19 @@
 // BannerExampleViewController.m
 #import "GADBannerView.h"
 #import "GADBannerViewDelegate.h"
+#import <iAd/iAd.h>
 
 extern UIView *gView;
 extern UIViewController *gViewColtroller;
 extern void init_controllers();
 
-@interface MyBanner : GADBannerView <GADBannerViewDelegate> {}
+@interface myBanner : NSObject <GADBannerViewDelegate, ADBannerViewDelegate>
+@property (nonatomic) BOOL show_ads;
+@property (strong, nonatomic) ADBannerView *ibanner;
+@property (strong, nonatomic) GADBannerView *gbanner;
 @end
 
-static MyBanner *banner = nil;
+static myBanner *vbanner = nil;
 
 void buy_full_version() {
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"itms://itunes.apple.com/app/eat-the-whistle/id581355601"]];    
@@ -22,85 +26,130 @@ int has_full_version() {
 extern void DoPause();
 extern BOOL game_start, pause_mode;
 
-@implementation MyBanner
+@implementation myBanner
+
+-(id)init {
+// admob allocation
+    NSLog(@"Creating google banner object");
+    if (!gViewColtroller)
+        init_controllers();
+    
+    // Create a view of the standard size at the top of the screen.
+    // Available AdSize constants are explained in GADAdSize.h.
+    self.gbanner = [[GADBannerView alloc] initWithAdSize:kGADAdSizeBanner];
+    
+    [self.gbanner setDelegate:self];
+    // Specify the ad's "unit identifier." This is your AdMob Publisher ID.
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+        self.gbanner.adUnitID = @"a15137065c6c467";
+    else
+        self.gbanner.adUnitID = @"a151370265aa576";
+    
+    self.gbanner.hidden = TRUE;
+    // Let the runtime know which UIViewController to restore after taking
+    // the user wherever the ad goes and add it to the view hierarchy.
+    self.gbanner.rootViewController = gViewColtroller;
+    [gView addSubview:self.gbanner];
+    
+// iad allocation
+    NSLog(@"Creating apple banner object");
+    self.ibanner = [[ADBannerView alloc] initWithFrame:CGRectZero];
+    self.ibanner.delegate = self;
+//    [self.ibanner sizeFromBannerContentSizeIdentifier:ADBannerContentSizeIdentifier320x50];
+    self.ibanner.hidden = TRUE;
+    [gView addSubview:self.ibanner];
+    self.show_ads = TRUE;
+    
+    return self;
+}
+// *** Google related methods
 
 - (void)adViewWillPresentScreen:(GADBannerView *)bannerView {
     if (game_start && !pause_mode)
         DoPause();
 }
-/*
-- (void)adViewDidDismissScreen:(GADBannerView *)bannerView {
-    NSLog(@"Did dismiss screen");
-}
-- (void)adViewWillDismissScreen:(GADBannerView *)bannerView {
-    NSLog(@"Will dismiss screen");
-}
-*/
+
 - (void)adViewWillLeaveApplication:(GADBannerView *)bannerView {
     if (game_start && !pause_mode)
         DoPause();
 }
 
-@end
-void show_ads(int ontop) {
-    if (!banner) {
-        NSLog(@"Creating banner object");
-        if (!gViewColtroller)
-            init_controllers();
-        
-        // Create a view of the standard size at the top of the screen.
-        // Available AdSize constants are explained in GADAdSize.h.
-        banner = [[MyBanner alloc] initWithAdSize:kGADAdSizeBanner];
-        
-        [banner setDelegate:banner];
-        // Specify the ad's "unit identifier." This is your AdMob Publisher ID.
-        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-            banner.adUnitID = @"a15137065c6c467";
-        else
-            banner.adUnitID = @"a151370265aa576";
-        
-        // Let the runtime know which UIViewController to restore after taking
-        // the user wherever the ad goes and add it to the view hierarchy.
-        banner.rootViewController = gViewColtroller;
-    }
+// Called before ad is shown, good time to show the add
+- (void)adViewDidReceiveAd:(GADBannerView *)view
+{
+    NSLog(@"Admob load");
+    self.ibanner.hidden = self.show_ads;
+    self.gbanner.hidden = FALSE;
+}
+
+// An error occured
+- (void)adView:(GADBannerView *)view didFailToReceiveAdWithError:(GADRequestError *)error
+{
+    NSLog(@"Admob error: %@", error);
+    self.gbanner.hidden = TRUE;
+}
+
+// **** iAd related methods
+- (void)bannerViewWillLoadAd:(ADBannerView *)banner
+{
+    NSLog(@"iAd load");
+    self.gbanner.hidden = self.show_ads;
+    self.ibanner.hidden = FALSE;
+}
+// Called when an error occured
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    NSLog(@"iAd error: %@", error);
+    self.ibanner.hidden = TRUE;
+    [self.gbanner loadRequest:[GADRequest request]];
+}
+
+-(void)dealloc {
+    NSLog(@"Freeing ads");
+    [self.gbanner setDelegate:nil];
+    self.ibanner.delegate = nil;
+    [self.gbanner removeFromSuperview];
+    [self.ibanner removeFromSuperview];
+    [self.gbanner release];
+    [self.ibanner release];
+    [super dealloc];
+}
+
+
+- (void)showAds:(int)ontop {
+    self.show_ads = TRUE;
     
     NSLog(@"Displaying banner object ontop:%d.", ontop);
-    GADRequest *request = [GADRequest request];
-#ifndef DEBUG_DISABLED
-    request.testDevices =  [NSArray arrayWithObjects:GAD_SIMULATOR_ID, nil];
-#endif
-    [gView addSubview:banner];
-    [banner loadRequest:request];
     
-    // start with an animation
     CGSize AdSize = kGADAdSizeBanner.size;
+
     
-    CGRect frame = banner.frame;
-    if (ontop)
-        frame.origin.y = -AdSize.height;
-    else
-        frame.origin.y = gViewColtroller.view.bounds.size.height;
-    
+    CGRect frame = self.gbanner.frame;
     frame.origin.x = (gViewColtroller.view.bounds.size.width - AdSize.width) / 2 ;
-    
-    banner.frame = frame;
-    
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:0.5];
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
-    
-    frame = banner.frame;
     
     if (ontop)
         frame.origin.y = 0.0f;
     else
         frame.origin.y = gViewColtroller.view.bounds.size.height - AdSize.height;
-        
-    banner.frame = frame;
-    [UIView commitAnimations];
+    
+    self.gbanner.frame = frame;
+    self.ibanner.frame = frame;
+}
+@end
+
+void show_ads(int ontop) {
+    if (!vbanner)
+        vbanner = [[myBanner alloc] init];
+    
+    [vbanner showAds:ontop];
 }
 
 void hide_ads() {
+    if (vbanner) {
+        [vbanner release];
+        vbanner = nil;
+    }
+    /*
     if (banner) {
         NSLog(@"Destroying banner object");
         [UIView animateWithDuration:0.5
@@ -116,8 +165,10 @@ void hide_ads() {
          {
              [banner setDelegate:nil];
              [banner removeFromSuperview];
+             [banner release];
              banner = nil;
          }];
     }
+     */
 }
 
